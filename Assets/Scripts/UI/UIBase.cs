f5583fa91a0d6ce37467a324f4ab0602e3a29d0e
yilvxingguang <1309690907@qq.com>
2022-03-27 10:24:36 +0800

    123

---
+++ Assets/Scripts/UI/UIBase.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+namespace XXFreamWork
+{
+
+    /// <summary>
+    /// UI基类
+    /// </summary>
+    public class UIBase : IEventHandler
+    {
+        public void HandleEvent(EventBase evt)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool HasBubbleUpHandlers()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool HasTrickleDownHandlers()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SendEvent(EventBase e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
+
+
---
+++ Library/PackageCache/com.unity.plasticscm-cn@1.1.8-preview/Editor/AutoSetup/PlasticWindowAutoOpen.cs
@@ -0,0 +1,25 @@
+﻿using Unity.PlasticSCM.Editor.UI;
+using UnityEditor;
+
+namespace Unity.PlasticSCM.Editor.AutoSetup
+{
+    [InitializeOnLoad]
+    internal class PlasticWindowAutoOpen
+    {
+        static PlasticWindowAutoOpen()
+        {
+            EditorApplication.update += RunOnce;
+        }
+
+        static void RunOnce()
+        {
+            EditorApplication.update -= RunOnce;
+            Execute();
+        }
+        
+        static void Execute()
+        {
+            ShowWindow.Plastic(false);
+        }
+    }
+}
